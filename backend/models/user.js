80932ef5f11568a34a36456c9dd891de37562899
rekha0suthar <rekha0suthar@gmail.com>
2024-11-30 15:53:54 +0530

    user schema defined

---
+++ backend/models/user.js
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  lastLoggedIn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified(this.password)) {
+    this.password = bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.checkPassword = function (password) {
+  return bcrypt.compare(this.password, password);
+};
+
+userSchema.methods.updateLogin = function () {
+  return this.model('User').findOneAndUpdate(
+    { email: this.email },
+    { lastLoggedIn: new Date() }
+  );
+};
+
+const User = model('User', userSchema);
+
+export default User;
