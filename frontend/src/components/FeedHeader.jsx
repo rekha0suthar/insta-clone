b8f9c79e5e2cb10ac1094d99983fd0b40a374a2d
rekha0suthar <rekha0suthar@gmail.com>
2024-12-08 18:00:17 +0530

    vaidation applied

--- frontend/src/components/FeedHeader.jsx
+++ frontend/src/components/FeedHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { BsThreeDots } from 'react-icons/bs';
 import { Context } from '../Context/Context';
 import AddPost from './AddPost';
@@ -28,35 +28,39 @@ const FeedHeader = ({ feed }) => {
 
   return (
     <>
-      <div className="user-header">
-        <img
-          src="https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI="
-          alt=""
-        />
-        <span>
-          <h2>{feed.userId.name}</h2>
-          <p>{feed.userId.address}</p>
-        </span>
-        {feed.userId._id !== userId && (
-          <button
-            className="follow-btn"
-            onClick={() => followOrUnfollow(feed.userId?._id)}
-          >
-            {user.friendList.includes(feed.userId?._id) ? 'Unfollow' : 'Follow'}
-          </button>
-        )}
-        {feed.userId._id === userId && (
-          <div className="feed-menu" onClick={() => setShowMenu(!showMenu)}>
-            <BsThreeDots />
-          </div>
-        )}
-        {showMenu && (
-          <div className="options">
-            <p onClick={() => editHandler(feed._id)}>Edit</p>
-            <p onClick={() => deleteFeed(feed._id)}>Delete</p>
-          </div>
-        )}
-      </div>
+      {feed && (
+        <div className="user-header">
+          <img
+            src="https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI="
+            alt=""
+          />
+          <span>
+            <h2>{feed.userId.name}</h2>
+            <p>{feed.userId.address}</p>
+          </span>
+          {feed.userId._id !== userId && (
+            <button
+              className="follow-btn"
+              onClick={() => followOrUnfollow(feed.userId?._id)}
+            >
+              {user.friendList?.includes(feed.userId?._id)
+                ? 'Unfollow'
+                : 'Follow'}
+            </button>
+          )}
+          {feed.userId?._id === userId && (
+            <div className="feed-menu" onClick={() => setShowMenu(!showMenu)}>
+              <BsThreeDots />
+            </div>
+          )}
+          {showMenu && (
+            <div className="options">
+              <p onClick={() => editHandler(feed._id)}>Edit</p>
+              <p onClick={() => deleteFeed(feed._id)}>Delete</p>
+            </div>
+          )}
+        </div>
+      )}
       {isShow && <AddPost feedId={id} />}
     </>
   );
