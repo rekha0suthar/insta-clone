321b95a348688f27bafeb6569f170aa0e2968b20
rekha0suthar <rekha0suthar@gmail.com>
2024-12-08 16:43:47 +0530

    env changed to prod

--- frontend/src/Context/Context.js
+++ frontend/src/Context/Context.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-const env = 'dev';
+const env = 'prod';
 
 const BASE_API_URI =
   env === 'dev'
